01590434111d1fa28f51ebac7dd15a768361b45f
Ricardo Mendes <ricardo.mends@gmail.com>
2016-03-29 16:48:41 +0100

    bug fixes

--- getDepSpaceToDeploy.sh
+++ getDepSpaceToDeploy.sh
@@ -1,16 +1,21 @@
 mkdir DepSpace;
 mkdir DepSpace/lib;
+mkdir DepSpace/bin;
 
 cp lib/DepSpace.jar lib/PVSS.jar lib/netty-3.1.1.GA.jar lib/SMaRt.jar lib/groovy-1.0-JSR-06.jar lib/commons-codec-1.5.jar DepSpace/lib;
+
+cp -r bin DepSpace;
 
 cp -r config DepSpace;
 
 echo -n "#!/bin/sh
 
-java -cp lib/DepSpace.jar:lib/PVSS.jar:lib/netty-3.1.1.GA.jar:lib/SMaRt.jar:lib/groovy-1.0-JSR-06.jar:lib/commons-codec-1.5.jar server.Main " > DepSpace/runDepSpace.sh;
+java -cp bin:lib/DepSpace.jar:lib/PVSS.jar:lib/netty-3.1.1.GA.jar:lib/SMaRt.jar:lib/groovy-1.0-JSR-06.jar:lib/commons-codec-1.5.jar server.Main " > DepSpace/runDepSpace.sh;
 
 echo -n "$" >> DepSpace/runDepSpace.sh;
 echo -n "1"  >> DepSpace/runDepSpace.sh;
+
+chmod 700 DepSpace/runDepSpace.sh;
 
 zip -r DepSpace.zip DepSpace;
 
